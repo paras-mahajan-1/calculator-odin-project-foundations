f377658ee1846fc00b9b6b75a5c9529a04c56931
Paras Mahajan <parasmahajan1010@gmail.com>
2025-09-28 15:13:53 +0530

    BASIC CALCULATOR FUNCTION DONE

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 console.log("It's working");
 
-
-while (true) {
-    let input = prompt("Enter the number!");
-
-    if (!input) {
-        break
-    };
-
-    let emptyArr = [];
-    emptyArr.push(input);
-    if ((emptyArr.length) == 1) {
-        console.log(emptyArr[0]);
-    };
+function calculator() {
     let counterVar = 0;
-    if (typeof input === 'number') {
-        counterVar = input;
+    let resultant;
+    if (counterVar == 0) {
+        let getInput = +prompt("Enter the first number");
+        resultant = getInput;
     };
+    let holding;
+    while (true) {
 
-    let resultant = 0;
+    let getOperator = prompt("Enter the operator");
+    let getSecondInput = +prompt("Enter the next number");
     
-    if ((input === "+") || (input === "-") || (input === "*") || (input === "/")) {
-        console.log("present")
+
+    if (getOperator === "+") {
+        resultant = resultant + getSecondInput;
+    };
+    if (getOperator === "-") {
+        resultant = resultant - getSecondInput;
+    };
+    if (getOperator === "*") {
+        resultant = resultant * getSecondInput;
+    };
+    if (getOperator === "/") {
+        resultant = resultant / getSecondInput;
+    };
+    if ((getOperator == "=") || (getSecondInput == "=")){
+        console.log(holding);
+        break;
     }
-    
-
-    
+    holding = resultant;
+    console.log(holding);
+}   
 };
 
-
+calculator();
 
 
 
